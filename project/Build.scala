0bf87d60410e01783840d19fc72b36052ba2616c
pgaref <pgaref@macbook-air.lan>
2015-06-10 00:35:36 +0100

    Major Kundera change for compatibility with SparkProject - models need to be at lib folder: jar -cvf myEntities.jar models/

--- project/Build.scala
+++ project/Build.scala
@@ -8,8 +8,11 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    "com.impetus.client" % "kundera-cassandra" % "2.5",
+    // Add your project dependencies here - Kundera Latest PLAY compatible version = 2.5 !
+    "com.impetus.client" % "kundera-cassandra" % "2.9",
+    "org.apache.lucene" % "lucene-analyzers" % "3.6.0",
+    //"com.datastax.cassandra" % "cassandra-driver-core" % "2.0.3",
+    //"org.apache.lucene" % "lucene-analyzers-common" % "4.0.0",
     javaCore
   )
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -10,3 +10,5 @@ addSbtPlugin("play" % "sbt-plugin" % "2.1.5")
 // eclipse plugin -- use the command: sbt eclipse
 // alternative option: play command and: eclipse with-source=true
 //addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.5")
+
+//unmanagedJars = ( baseDirectory / "target/scala-2.10/classes" )
