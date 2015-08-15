d6967bb30bf382d908010218c3a4c7b60b83dd71
Sokratis Dafnis <sokratis.dafnis@gmail.com>
2015-08-15 22:31:39 +0300

    Adding angular in project

---
+++ public/app/main/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('myApp.main', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/main', {
+    templateUrl: '/public/app/main/main.html',
+    controller: 'MainCtrl'
+  });
+}])
+
+.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
+	$http.get('/playlists').
+    success(function(data, status, headers, config) {
+      	$scope.playlists = data;
+      	console.log(data);
+    }).
+    error(function(data, status, headers, config) {
+      console.error(status);
+    });
+}]);
