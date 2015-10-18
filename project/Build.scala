65e7fcde2a2837a7fed307889659a9b6d0e9a65e
pgaref <pgaref@macbook-air.lan>
2015-10-18 05:17:28 +0100

    Datastax dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -8,10 +8,9 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here - Kundera Latest PLAY compatible version = 2.5 !
-    "com.impetus.client" % "kundera-cassandra" % "2.9",
     "org.apache.lucene" % "lucene-analyzers" % "3.6.0",
-    //"com.datastax.cassandra" % "cassandra-driver-core" % "2.0.3",
+    "com.datastax.cassandra" % "cassandra-driver-core" % "2.1.8",
+    "com.datastax.cassandra" % "cassandra-driver-mapping" % "2.1.8",
     //"org.apache.lucene" % "lucene-analyzers-common" % "4.0.0",
     javaCore
   )
@@ -19,9 +18,7 @@ object ApplicationBuild extends Build {
   val main = play.Project(appName, appVersion, appDependencies).settings(
       //Kundera Public repositories
       ebeanEnabled := false,
-      resolvers += "Kundera" at "https://oss.sonatype.org/content/repositories/releases",
       resolvers += "Riptano" at "http://mvn.riptano.com/content/repositories/public",
-      resolvers += "Kundera missing" at "http://kundera.googlecode.com/svn/maven2/maven-missing-resources",
       resolvers += "Scale 7" at "https://github.com/s7/mvnrepo/raw/master"
   )
 
