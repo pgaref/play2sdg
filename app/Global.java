b9d3f71e6c4c40e3eae379b13c9eb0929a6a5c0f
pgaref <pgaref@macbook-air.lan>
2015-10-18 03:36:21 +0100

    Reading cluster configuration from file using ConfigFactory

--- app/Global.java
+++ app/Global.java
@@ -1,11 +1,32 @@
+import java.util.List;
+
+import com.typesafe.config.ConfigFactory;
+
+import controllers.CassandraDxQueryController;
+import managers.ClusterManager;
 import play.Application;
 import play.GlobalSettings;
+import play.Logger;
 
 public class Global extends GlobalSettings {
     @Override
     public void onStart(Application app) {
-       /* if (User.find.findRowCount() == 0) {
-            Ebean.save((List<?>) Yaml.load("initial-data.yml"));
-        }*/
+    	
+    	List<String> hosts = ConfigFactory.load().getStringList("cassandra.nodes");
+    	Logger.debug("Cassandra hosts from Config: "+ hosts.toString());
+    	if(hosts == null || hosts.size() == 0 ){
+    		Logger.error("Cassandra HOSTs argument cannot be null!");
+    		System.exit(-1);
+    	}
+    	else{
+    		controllers.Application.clusterManager = new ClusterManager("play_cassandra", 1, hosts.toArray(new String[hosts.size()]));
+    		controllers.Application.dxController = new CassandraDxQueryController(controllers.Application.clusterManager.getSession());
+    	}
+    }
+    
+    @Override
+    public void onStop(Application app) {
+    	Logger.info("Shutting down Cassandra cluster Connections..");
+    	controllers.Application.clusterManager.disconnect();
     }
 }
