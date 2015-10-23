894b7471af0792453619d02ce6ff86efec0eb7e4
Panagiotis Garefalakis <pangaref@gmail.com>
2015-10-23 19:06:06 +0100

    script to run play application

---
+++ benchmark/scripts/run_play.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Author: pg1712@imperial.ac.uk - pgaref.github.io
+# Automating the Play-Spark test process
+# -------------------------------------------------
+
+_PORT=9000
+
+
+_term() { 
+  echo "Caught SIGTERM signal!" 
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
+
+exec /bin/sh /home/pg1712/play2sdg-1.0-SNAPSHOT/start -Dhttp.port=$_PORT &
+
+child=$! 
+wait "$child"
+
+echo "Should never Terminate!!"
