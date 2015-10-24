46dd77dd15184f94002a56be07448c03a392c861
pgaref <pgaref@macbook-air.lan>
2015-10-24 19:25:20 +0100

    Updating start services scripts

--- benchmark/scripts/run_play.sh
+++ benchmark/scripts/run_play.sh
@@ -13,7 +13,7 @@ _term() {
 
 trap _term SIGTERM
 
-
+rm /home/pg1712/play2sdg-1.0-SNAPSHOT/RUNNING_PID
 exec /bin/sh /home/pg1712/play2sdg-1.0-SNAPSHOT/start -Dhttp.port=$_PORT &
 
 child=$! 
--- benchmark/scripts/run_spark_cf_continously.sh
+++ benchmark/scripts/run_spark_cf_continously.sh
@@ -3,10 +3,17 @@
 # Automating the Play-Spark test process
 # -------------------------------------------------
 
+_term() { 
+  echo "Caught SIGTERM signal!" 
+  kill -TERM "$child" 2>/dev/null
+}
+
 while :
 do
+#    trap _term SIGTERM
 	/home/pg1712/spark-1.2.0-bin-cdh4/bin/spark-submit --master local[*] --driver-memory 1g --class main.java.uk.ac.imperial.lsds.play2sdg.SparkCollaborativeFiltering /home/pg1712/play2sdg-Spark-module-1.2-driver.jar local[*] 192.168.0.16
-
+#    child=$! 
+#    wait "$child"
 	echo "Spark CF Job done! Repeat!"
 done
 
